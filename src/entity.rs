6fc0a0230ff18a746fa58e8fd7d4d6ea008d40c4
Keyon <keyonh@gmail.com>
2021-07-05 15:17:55 -0400

    death function

--- src/entity.rs
+++ src/entity.rs
@@ -20,91 +20,6 @@ pub trait Health {
     fn death(&mut self) -> bool;
 }
 
-<<<<<<< HEAD
-// pub struct Speed {
-//     pub pos: Vec2<f32>,
-//     pub hitbox: Vec2<u32>, // Hitbox where speed enemy takes damage.
-//     pub walkbox: Rect, // Hitbox involved in collision with rooms.
-//     pub speed: f32,
-//     pub dir: Direction,
-//     pub hp: i32,    //store the health for speed enemy
-//     pub m_hp: i32,
-//     pub last_dir_update: Option< Instant >
-// }
-
-// impl Health for Speed {
-//     fn max_hp(&self) -> i32 { self.m_hp }
-//     fn health(&self) -> i32 { self.hp }
-//     fn damage(&mut self, d: i32) -> i32 {
-//         self.hp = (self.hp - d).max(0);
-//         self.hp
-//     }
-
-//     fn heal(&mut self, h: i32) -> i32 {
-//         self.hp = (self.hp + h).min(self.m_hp);
-//         self.hp
-//     }
-// }
-
-// impl Speed {
-//     pub fn new(position: Vec2<f32>) -> Speed {
-//         Speed {
-//             pos: position,
-//             hitbox: Vec2::new(40, 40),
-//             walkbox: Rect::new(20, 12, 40, 24),
-//             speed: 4.0,
-//             dir: Direction::Left,
-//             hp: 2,
-//             m_hp: 2,
-//         }
-//     }
-
-//     Using Connor's player implementation for this design:
-
-//     pub fn get_pos_x(&self) -> i32 { self.pos.x as i32 }
-//     pub fn get_pos_y(&self) -> i32 { self.pos.y as i32 }
-
-//     pub fn get_walkbox(&self) -> Rect { self.walkbox }
-//     pub fn get_walkbox_world(&self) -> Rect {
-//         Rect::new(
-//             self.pos.x as i32 - self.walkbox.x(),
-//             self.pos.y as i32 - self.walkbox.y(),
-//             self.walkbox.width(),
-//             self.walkbox.height(),
-//         )
-//     }
-
-//     pub fn update_pos(& mut self) {
-//         let now = Instant::new();
-
-//         let mut rng = rand::thread_rng();
-//         let mut mov_vec = Vec2::new(0.0, 0.0);
-
-//         match self.last_dir_update {
-//             Some(update_time) => {
-//                 if update_time.elapsed() >= Duration::seconds(3) {
-//                     mov_vec.x = rng.gen_range(-1.0, 1.0);
-//                     mov_vec.y = rng.gen_range(-1.0, 1.0);
-//                     self.last_dir_update = Some(now);
-//                 }
-//             },
-//             None( .. ) => {
-//                 self.last_dir_update = Some(now);
-//             }
-//         }
-
-//         // Update position using movement vector and speed
-//         self.pos.x += mov_vec.x * self.speed;
-//         self.pos.y += mov_vec.y * self.speed;
-//     }
-
-//     pub fn get_hitbox_x(&self) -> u32 { self.hitbox.x }
-//     pub fn get_hitbox_y(&self) -> u32 { self.hitbox.y }
-
-//     pub fn set_dir(& mut self, new_dir: Direction) { self.dir = new_dir; }
-//     pub fn get_dir(& mut self) -> Direction { self.dir }
-// }
-=======
 pub enum EnemyKind {
     Attack,
     Health,
@@ -162,7 +77,7 @@ impl Enemy {
     pub fn get_pos_y(&self) -> i32 { self.pos.y as i32 }
 
     pub fn get_walkbox(&self) -> Rect { self.walkbox }
-    pub fn get_walkbox_world(&self) -> Rect { 
+    pub fn get_walkbox_world(&self) -> Rect {
         Rect::new(
             self.pos.x as i32 - self.walkbox.x(),
             self.pos.y as i32 - self.walkbox.y(),
@@ -239,4 +154,3 @@ impl Enemy {
     pub fn set_dir(& mut self, new_dir: Direction) { self.dir = new_dir; }
     pub fn get_dir(& mut self) -> Direction { self.dir }
 }
->>>>>>> main
